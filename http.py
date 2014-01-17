6a8fb3b94203b1dcc7f37b5d950225910c7f5d83
Mathieu Lecarme <mlecarme@bearstech.com>
2014-01-17 15:37:48 +0100

    Doc.

--- http.py
+++ http.py
@@ -4,6 +4,17 @@ import dpkt
 
 
 class HTTPReader(object):
+    """
+    Iterate over a dpkt pcap reader.
+
+    Request and its response are grouped.
+    Yield lots of informations :
+        * source_destination: source ip and ort, destination ip and port
+        * timers: starting request, starting response, ending response
+        * request
+        * response
+    """
+
     def __init__(self, pcap, port=80):
         self.pcap = pcap
         self.port = port
@@ -15,13 +26,16 @@ class HTTPReader(object):
         for ts, buf in self.pcap:
             eth = dpkt.ethernet.Ethernet(buf)
             if not isinstance(eth.data, dpkt.ip.IP):
+                # I want IP
                 continue
             ip = eth.data
             if not isinstance(ip.data, dpkt.tcp.TCP):
+                # I want TCP
                 continue
             tcp = ip.data
             if (tcp.dport == self.port or
                     tcp.sport == self.port) and len(tcp.data) > 0:
+                # Not empty packet, on the right port
                 is_request = tcp.dport == self.port
                 bk = (ip.src, tcp.sport, ip.dst, tcp.dport)  # Buffer key
                 if bk not in self.buffers:
@@ -44,10 +58,11 @@ class HTTPReader(object):
                         self.requests[(ip.src, tcp.sport,
                                        ip.dst, tcp.dport)] = http
                     else:
-                        rk = (ip.dst, tcp.dport, ip.src, tcp.sport)  # request key
+                        # request key
+                        rk = (ip.dst, tcp.dport, ip.src, tcp.sport)
                         if rk in self.requests:
-                            yield (socket.inet_ntoa(ip.dst), tcp.dport,
-                                   socket.inet_ntoa(ip.src), tcp.sport
+                            yield (socket.inet_ntoa(ip.src), tcp.sport,
+                                   socket.inet_ntoa(ip.dst), tcp.dport
                                    ), (self.timers[bk], self.timers[rk], ts
                                        ), self.requests[rk], http
 
