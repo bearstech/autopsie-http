942834a1bf8f1ec3465b6579dab84ba44ce05c9d
Mathieu Lecarme <mlecarme@bearstech.com>
2014-01-17 14:58:59 +0100

    Cleanup.

--- http.py
+++ http.py
@@ -37,31 +37,21 @@ class HTTPReader(object):
                 else:
                     del self.buffers[bk]
                     if tcp.dport == 80:
-                        self.requests[(ip.src, tcp.sport, ip.dst, tcp.dport)] = http
+                        self.requests[(ip.src, tcp.sport,
+                                       ip.dst, tcp.dport)] = http
                     else:
                         rk = (ip.dst, tcp.dport, ip.src, tcp.sport)
                         if rk in self.requests:
-                            yield self.requests[rk], http
-                            #print "%s:%i -> %s:%i" % (socket.inet_ntoa(ip.dst),
-                                                    #tcp.dport,
-                                                    #socket.inet_ntoa(ip.src),
-                                                    #tcp.sport)
-                            #print "http://%s/%s" % (requests[rk].headers['host'],
-                                                    #requests[rk].uri)
-                            #print "latency:", ts - timers[bk]
-                            #print "\t", requests[rk].headers
-                            #print
-                            #print "Status:", http.status
-                            #print "Chrono:", ts - timers[rk]
-                            #print "\t", http.headers
-                            #print
-                            #print
+                            yield (socket.inet_ntoa(ip.dst), tcp.dport,
+                                   socket.inet_ntoa(ip.src), tcp.sport
+                                   ), (self.timers[bk], self.timers[rk], ts
+                                       ), self.requests[rk], http
 
 if __name__ == '__main__':
     import sys
 
     f = open(sys.argv[1], 'r')
     pcap = dpkt.pcap.Reader(f)
-    for request, response  in HTTPReader(pcap):
+    for source_destination, timers, request, response in HTTPReader(pcap):
         print request.headers
         print response.headers
