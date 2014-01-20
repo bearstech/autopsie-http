c163a43787a067055dd2ba67f67574ffc2d9138d
Mathieu Lecarme <mlecarme@bearstech.com>
2014-01-20 11:15:03 +0100

    Live capture.

--- http.py
+++ http.py
@@ -20,7 +20,7 @@ class HTTPReader(object):
         self.port = port
         self.buffers = {}
         self.requests = {}
-        self.request_timers = {}
+        self.request_start_timers = {}
         self.request_end_timers = {}
         self.response_timers = {}
 
@@ -40,18 +40,19 @@ class HTTPReader(object):
                 # Not empty packet, on the right port
                 is_request = tcp.dport == self.port
                 bk = (ip.src, tcp.sport, ip.dst, tcp.dport)  # Buffer key
-                # request key
-                rk = (ip.dst, tcp.dport, ip.src, tcp.sport)
+                rk = (ip.dst, tcp.dport, ip.src, tcp.sport)  # reverse key
                 if bk not in self.buffers:
                     self.buffers[bk] = tcp.data
                 else:
                     self.buffers[bk] += tcp.data
+
                 if is_request:
-                    if bk not in self.request_timers:
-                        self.request_timers[bk] = ts
+                    if bk not in self.request_start_timers:
+                        self.request_start_timers[bk] = ts
                 else:
                     if bk not in self.response_timers:
                         self.response_timers[bk] = ts
+
                 try:
                     if is_request:
                         http = dpkt.http.Request(self.buffers[bk])
@@ -65,14 +66,15 @@ class HTTPReader(object):
                     if is_request:
                         self.requests[bk] = http
                         self.request_end_timers[bk] = ts
+                        assert bk in self.request_start_timers
                     else:
                         if rk in self.requests:
-                            assert self.request_timers[rk] <= self.response_timers[bk]
-                            assert self.response_timers[bk] <= ts
-                            request_start = self.request_timers[rk]
+                            #assert self.request_timers[rk] <= self.response_timers[bk]
+                            #assert self.response_timers[bk] <= ts
+                            request_start = self.request_start_timers[rk]
                             request_end = self.request_end_timers[rk]
                             response_start = self.response_timers[bk]
-                            del self.request_timers[rk]
+                            del self.request_start_timers[rk]
                             del self.request_end_timers[rk]
                             del self.response_timers[bk]
                             yield (socket.inet_ntoa(ip.src), tcp.sport,
@@ -83,9 +85,23 @@ class HTTPReader(object):
 
 if __name__ == '__main__':
     import sys
+    import os
 
-    f = open(sys.argv[1], 'r')
-    pcap = dpkt.pcap.Reader(f)
+    # Don't buffer on STDOUT
+    sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+
+    if sys.argv[1] == '-i':
+        import pcap as _pcap
+        pcap = _pcap.pcap(sys.argv[2])
+        if len(sys.argv) > 3:
+            pcap.setfilter(' '.join(sys.argv[3:]))
+        print("listening on %s" % sys.argv[2])
+    else:
+        f = open(sys.argv[1], 'r')
+        pcap = dpkt.pcap.Reader(f)
     for source_destination, timers, request, response in HTTPReader(pcap):
-        print request.headers
-        print response.headers
+        sys.stdout.write("\n")
+        print((timers[3] - timers[0]) * 1000)
+        print(request.headers)
+        print(response.headers)
+        #sys.stdout.flush()
